9608bcd14fbcc0a4c872e47c77212501739ea4f8
raghavshringi <84003735+raghavshringi@users.noreply.github.com>
2021-05-19 23:18:05 +0530

    Add files via upload

---
+++ using_variable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 4;
+    // hERE I write a program of how to use a variable in c     
+
+    float b = 8.5;
+    char c = 'p';
+    int d = 45;
+    int e = 45 + 4;
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %f \n", b);
+    printf("The value of c is %c \n", c);
+    printf("Sum of a and d is %d \n", a + d);
+    printf("Sum of a and d is %d \n", e );
+
+    return 0;
+}
