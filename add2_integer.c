7fb6f8d9f948a0f46ca2300761728aa8e0ced040
raghavshringi <84003735+raghavshringi@users.noreply.github.com>
2021-05-19 23:22:17 +0530

    Add files via upload

---
+++ add2_integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+   int var2;
+   int var3;
+   int var4;
+   var2=60;
+   var3=40;
+   var4=var2+var3;
+   printf("Sum of %d and %d is %d", var2, var3,var4);
+   return 0;
+
+}
