d1fc2d048829ec9894f2bd75d80da3868bdbea85
raghavshringi <84003735+raghavshringi@users.noreply.github.com>
2021-05-19 23:23:40 +0530

    Add files via upload

---
+++ 2variablevalues.c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <stdio.h>
+
+// Function to find the square-root of N
+double findSQRT(double N) { return sqrt(N); }
+
+int main()
+{
+	// Given number
+	int N = 12;
+
+	// Function call
+	printf("%f ", findSQRT(N));
+	return 0;
+}
