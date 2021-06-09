f697dce234258d8773548f761086b73dee450ce9
raghavshringi <84003735+raghavshringi@users.noreply.github.com>
2021-06-09 14:02:51 +0530

    Create To_find_quotient_and_reminder.c

---
+++ To_find_quotient_and_reminder.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+    {
+     int a,b,c,d; 
+    printf("enter value of a,b:");
+   
+     scanf("%d%d",&a,&b);
+     c=a/b; 
+    d=a%b;
+     printf("quotient:%d",c);
+     printf("reminder:%d",d); 
+    return 0;
+    }
