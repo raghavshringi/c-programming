961b09552d60eeab76ad181c40bb8362e54548de
raghavshringi <84003735+raghavshringi@users.noreply.github.com>
2021-06-09 14:06:55 +0530

    Create Area_and_perimeter_of_circle.c

---
+++ Area_and_perimeter_of_circle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+    {
+    	int r;
+    	double pi=3.14,area,ci;
+    	printf("\nenter r of circle:");
+    	scanf("%d", &r);
+    	area=pi*r*r;
+    	printf("\n area of circle: %lf", area);
+    	ci=2*pi*r;	
+    printf("\n ci: %lf",ci);	
+    return 0;	
+    	}
